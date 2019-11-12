855133e26d8b3758f13bd76b5cda3d876466dc93
Evan Greco <e.greco12@gmail.com>
2019-11-12 14:42:44 -0600

    Fix indentation

--- app/index.js
+++ app/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom'
 import './index.css'
 
 class App extends React.Component {
-  render () {
-	return <div>Hello World!</div>
-  }
+	render () {
+		return <div>Hello World!</div>
+	}
 }
 
 ReactDOM.render(<App />, document.getElementById('app'))
