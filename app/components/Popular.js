16b50e2f1ec470fc4be6f7a1bd91664172d84ede
Evan Greco <e.greco12@gmail.com>
2019-11-13 14:05:39 -0600

    Cache gh results

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -2,28 +2,29 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { fetchPopularRepos } from '../utils/api';
 
-function LanguagesNav ({ selected, onUpdateLanguage }) {
+function LangaugesNav ({ selected, onUpdateLanguage }) {
     const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
-    
-	return (
-		<ul className='flex-center'>
+
+    return (
+        <ul className='flex-center'>
           {languages.map((language) => (
               <li key={language}>
-                <button className='btn-clear nav-link'
-                        style={language === selected ? { color: 'rgb(187, 46, 31'} : null}
-                        onClick={() => onUpdateLanguage(language)}>
+                <button
+                  className='btn-clear nav-link'
+                  style={language === selected ? { color: 'rgb(187, 46, 31)' } : null}
+                  onClick={() => onUpdateLanguage(language)}>
                   {language}
                 </button>
               </li>
           ))}
         </ul>
-	);
+    );
 }
 
-LanguagesNav.propTypes = {
+LangaugesNav.propTypes = {
     selected: PropTypes.string.isRequired,
     onUpdateLanguage: PropTypes.func.isRequired
-};
+}
 
 export default class Popular extends React.Component {
     constructor(props) {
@@ -31,61 +32,62 @@ export default class Popular extends React.Component {
 
         this.state = {
             selectedLanguage: 'All',
-            repos: null,
-            error: null
+            repos: {},
+            error: null,
         };
 
         this.updateLanguage = this.updateLanguage.bind(this);
         this.isLoading = this.isLoading.bind(this);
     }
-
-
     componentDidMount () {
         this.updateLanguage(this.state.selectedLanguage);
     }
-    
     updateLanguage (selectedLanguage) {
         this.setState({
             selectedLanguage,
             error: null,
-            repos: null
         });
 
-        fetchPopularRepos(selectedLanguage)
-            .then((repos) => this.setState({
-                repos,
-                error: null
-            })).catch(() => {
-                console.warn('Error fetching repos: ', error);
+        if (!this.state.repos[selectedLanguage]) {
+            fetchPopularRepos(selectedLanguage)
+                .then((data) => {
+                    this.setState(({ repos }) => ({
+                        repos: {
+                            ...repos,
+                            [selectedLanguage]: data
+                        }
+                    }));
+                })
+                .catch(() => {
+                    console.warn('Error fetching repos: ', error);
 
-                this.setState({
-                    error: `There was an error fetching the repos.`
+                    this.setState({
+                        error: `There was an error fetching the repositories.`
+                    });
                 });
-            });
+        }
     }
-
     isLoading() {
-        return this.state.repos === null && this.state.error === null;
+        const { selectedLanguage, repos, error } = this.state;
+
+        return !repos[selectedLanguage] && error === null;
     }
-    
-	render() {
+    render() {
         const { selectedLanguage, repos, error } = this.state;
 
         return (
             <React.Fragment>
-            <LanguagesNav
-              selected={selectedLanguage}
-              onUpdateLanguage={this.updateLanguage}
-            />
+              <LangaugesNav
+                selected={selectedLanguage}
+                onUpdateLanguage={this.updateLanguage}
+              />
 
               {this.isLoading() && <p>LOADING</p>}
 
               {error && <p>{error}</p>}
 
-              {repos && <pre>{JSON.stringify(repos, null, 2)}</pre>}
+              {repos[selectedLanguage] && <pre>{JSON.stringify(repos[selectedLanguage], null, 2)}</pre>}
             </React.Fragment>
         );
     }
-        
 }
-
